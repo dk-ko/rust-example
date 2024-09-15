fd5c14509157179a787bff62cf793cf7db4e676c
jade ko <dev.dkko@gmail.com>
2024-09-15 13:58:47 +0900

    refactor: 폴더 분리

--- src/bin/ch1_show_price.rs
+++
@@ -1,4 +0,0 @@
-fn main() {
-    let banana = 300;
-    println!("바나나 가격={}원", banana)
-}
--- src/bin/ch2_hex_oct_bin.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    let v1 = 0xFF;
-    let v2 = 0o655;
-    let v3 = 0b1101_0101;
-    println!("{},{},{}", v1, v2, v3);
-}
