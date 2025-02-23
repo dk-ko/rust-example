28dcc708aa0707bf007338bb56f665965d8ade3c
jade ko <dev.dkko@gmail.com>
2025-02-23 22:46:22 +0900

    ch3: cat 예제

---
+++ command-line-rust-example/03_catr/src/main.rs
@@ -0,0 +1,58 @@
+use anyhow::Result;
+use clap::Parser;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+struct Args {
+    #[arg(value_name = "FILE", default_value = "-")]
+    files: Vec<String>,
+
+    #[arg(short('n'), long("number"), conflicts_with("number_nonblank_lines"))]
+    number_lines: bool,
+
+    #[arg(short('b'), long("number-nonblank"))]
+    number_nonblank_lines: bool,
+}
+
+fn main() {
+    if let Err(e) = run(Args::parse()) {
+        eprintln!("{e}");
+        std::process::exit(1);
+    }
+}
+
+fn run(args: Args) -> Result<()> {
+    for filename in args.files {
+        match open(&filename) {
+            Err(err) => eprintln!("Failed to open {filename}: {err}"),
+            Ok(file) => {
+                let mut prev_num = 0;
+                for (line_num, line) in file.lines().enumerate() {
+                    let line = line?;
+                    if args.number_lines {
+                        println!("{:>6}\t{line}", line_num + 1);
+                    } else if args.number_nonblank_lines {
+                        if line.is_empty() {
+                            println!();
+                        } else {
+                            prev_num += 1;
+                            println!("{prev_num:>6}\t{line}");
+                        }
+                    } else {
+                        println!("{line}");
+                    }
+                }
+            },
+        }
+    }
+    Ok(())
+}
+
+fn open(filename: &str) -> Result<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
